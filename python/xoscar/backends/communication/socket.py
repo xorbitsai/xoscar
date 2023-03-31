3dc13a01293742205cc26fb4ebb43d2457f64ff2
UranusSeven <109661872+UranusSeven@users.noreply.github.com>
2023-03-30 19:33:08 -0700

    ENH: use /tmp/xoscar as default temp dir (#13)

--- python/xoscar/backends/communication/socket.py
+++ python/xoscar/backends/communication/socket.py
@@ -20,7 +20,7 @@ import concurrent.futures as futures
 import os
 import socket
 import sys
-import tempfile
+import warnings
 from abc import ABCMeta
 from asyncio import AbstractServer, StreamReader, StreamWriter
 from functools import lru_cache
@@ -29,6 +29,7 @@ from typing import Any, Callable, Coroutine, Dict, Type
 from urllib.parse import urlparse
 
 from ..._utils import to_binary
+from ...constants import XOSCAR_UNIX_SOCKET_DIR
 from ...serialization import AioDeserializer, AioSerializer, deserialize
 from ...utils import classproperty, implements
 from .base import Channel, ChannelType, Client, Server
@@ -258,12 +259,24 @@ class SocketClient(Client):
         return SocketClient(local_address, dest_address, channel)
 
 
-TEMPDIR = tempfile.gettempdir()
+def _get_or_create_default_unix_socket_dir():
+    os.makedirs(XOSCAR_UNIX_SOCKET_DIR, exist_ok=True)
+    try:
+        os.chmod(XOSCAR_UNIX_SOCKET_DIR, mode=0o777)
+    except PermissionError:  # pragma: no cover
+        warnings.warn(
+            "Lack of permission on the socket dir %s" % XOSCAR_UNIX_SOCKET_DIR,
+            RuntimeWarning,
+        )
+    return XOSCAR_UNIX_SOCKET_DIR
 
 
 @lru_cache(100)
 def _gen_unix_socket_default_path(process_index):
-    return f"{TEMPDIR}/xoscar/{md5(to_binary(str(process_index))).hexdigest()}"  # nosec
+    return (
+        f"{_get_or_create_default_unix_socket_dir()}/"
+        f"{md5(to_binary(str(process_index))).hexdigest()}"
+    )  # nosec
 
 
 @register_server
