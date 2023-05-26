193f943bf9d1721fc1358643203224c00bf1fa79
ChengjieLi <chengjieli23@outlook.com>
2023-05-26 14:15:29 +0800

    add more tests

--- python/xoscar/backends/test/tests/test_transfer.py
+++ python/xoscar/backends/test/tests/test_transfer.py
@@ -1,3 +1,17 @@
+# Copyright 2022-2023 XProbe Inc.
+# derived from copyright 1999-2021 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import asyncio
 import os
 import sys
@@ -144,6 +158,20 @@ async def _copy_test(scheme: str, cpu: bool):
         await actor_ref1.copy_data(actor_ref3, sizes, cpu=cpu)
         await actor_ref1.copy_data(actor_ref4, sizes, cpu=cpu)
         await actor_ref1.copy_data(actor_ref5, sizes, cpu=cpu)
+
+        # test small size of data
+        sizes = [
+            1 * 1024**2,
+            2 * 1024**2,
+            1 * 1024**2,
+        ]
+        tasks = [
+            actor_ref2.copy_data(actor_ref1, sizes, cpu=cpu),
+            actor_ref2.copy_data(actor_ref3, sizes, cpu=cpu),
+            actor_ref2.copy_data(actor_ref4, sizes, cpu=cpu),
+            actor_ref2.copy_data(actor_ref5, sizes, cpu=cpu),
+        ]
+        await asyncio.gather(*tasks)
 
 
 schemes: List[Optional[str]] = [None]
