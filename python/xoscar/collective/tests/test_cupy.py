30efafd091e5427a176db2ba67431687ee5a51eb
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2023-07-18 22:08:31 -0400

    add allreduce api

---
+++ python/xoscar/collective/tests/test_cupy.py
@@ -0,0 +1,53 @@
+# Copyright 2022-2023 XProbe Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import multiprocessing as mp
+
+import cupy
+import cupy.cuda.nccl as nccl
+
+from ...tests.core import require_unix
+
+
+def worker_allreduce(rank, nccl_id, device_id):
+    from .. import xoscar_cupy as xc
+
+    cupy.cuda.Device(device_id).use()
+
+    context = xc.Context(n_devices=2, nccl_unique_id=nccl_id, rank=rank)
+    sendbuf = cupy.array([[1, 2, 3], [1, 2, 3]], dtype=cupy.float32)
+    recvbuf = cupy.zeros_like(sendbuf, dtype=cupy.float32)
+    op = xc.ReduceOp.SUM
+
+    xc.allreduce(context, sendbuf, recvbuf, op)
+    cupy.testing.assert_array_equal(recvbuf, cupy.array(sendbuf * 2))
+
+
+@require_unix
+def test_allreduce():
+    mp.set_start_method('spawn')
+
+    nccl_id = nccl.get_unique_id()
+    device_id = [0, 1]
+
+    process1 = mp.Process(target=worker_allreduce, args=(0, nccl_id, device_id[0]))
+    process1.start()
+    process2 = mp.Process(target=worker_allreduce, args=(1, nccl_id, device_id[1]))
+    process2.start()
+
+    process1.join()
+    process2.join()
+
+    assert process1.exitcode == 0, "Process 1 encountered an error."
+    assert process2.exitcode == 0, "Process 2 encountered an error."
---
+++ python/xoscar/collective/xoscar_cupy.py
@@ -0,0 +1,83 @@
+# Copyright 2022-2023 XProbe Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum
+from typing import Optional, Tuple
+
+import cupy
+import cupy.cuda.nccl as nccl
+
+
+class ReduceOp(Enum):
+    SUM = nccl.NCCL_SUM
+    PRODUCT = nccl.NCCL_PROD
+    MIN = nccl.NCCL_MIN
+    MAX = nccl.NCCL_MAX
+
+
+NUMPY_NCCL_DTYPE_MAP = {
+    cupy.uint8: nccl.NCCL_UINT8,
+    cupy.uint32: nccl.NCCL_UINT32,
+    cupy.uint64: nccl.NCCL_UINT64,
+    cupy.int8: nccl.NCCL_INT8,
+    cupy.int32: nccl.NCCL_INT32,
+    cupy.int64: nccl.NCCL_INT64,
+    cupy.half: nccl.NCCL_HALF,
+    cupy.float16: nccl.NCCL_FLOAT16,
+    cupy.float32: nccl.NCCL_FLOAT32,
+    cupy.float64: nccl.NCCL_FLOAT64,
+    cupy.double: nccl.NCCL_DOUBLE
+}
+
+
+class Context:
+    def __init__(self, n_devices: int, nccl_unique_id: Tuple, rank: int):
+        self.communicator = nccl.NcclCommunicator(n_devices,nccl_unique_id, rank)
+
+
+def allreduce(
+    context: Optional[Context] = None,
+    sendbuf: Optional[cupy.ndarray] = None,
+    recvbuf: Optional[cupy.ndarray] = None,
+    reduceop: Optional[ReduceOp] = ReduceOp.SUM,
+):
+    context.communicator.allReduce(
+        get_buffer_ptr(sendbuf),
+        get_buffer_ptr(recvbuf),
+        get_buffer_n_elements(sendbuf),
+        get_nccl_buffer_dtype(sendbuf),
+        reduceop.value,
+        cupy.cuda.Stream.null.ptr,
+    )
+
+
+def get_buffer_ptr(buffer):
+    if isinstance(buffer, cupy.ndarray):
+        return buffer.data.ptr
+
+    raise ValueError("Buffer type not supported")
+
+
+def get_buffer_n_elements(buffer):
+    if isinstance(buffer, (cupy.ndarray)):
+        return buffer.size
+
+    raise ValueError("Buffer type not supported")
+
+
+def get_nccl_buffer_dtype(buffer):
+    if isinstance(buffer, cupy.ndarray):
+        return NUMPY_NCCL_DTYPE_MAP[buffer.dtype.type]
+
+    raise ValueError("Unspported GPU buffer type")
