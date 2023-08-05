2d9679e9bfe1ec7782a9c3129bc59f6c7b022487
Chengjie Li <109656400+ChengjieLi28@users.noreply.github.com>
2023-08-05 17:42:33 +0800

    ENH: add no_lock for avoiding possible dead lock during local actor ref method call (#57)

--- python/xoscar/tests/test_batch.py
+++ python/xoscar/tests/test_batch.py
@@ -18,7 +18,9 @@ import sys
 
 import pytest
 
+from .. import Actor, actor_ref, create_actor, create_actor_pool
 from ..batch import build_args_binder, extensible
+from ..core import NO_LOCK_ATTRIBUTE_HINT, no_lock
 
 
 def _wrap_async(use_async):
@@ -215,3 +217,74 @@ async def test_extensible_single_with_batch(use_async):
     assert ret == [3, 3]
     assert test_inst.arg_list == [(30,), (33,), (35,)]
     assert test_inst.kwarg_list == [{"kwarg": 112}, {"kwarg": 115}, {"kwarg": 117}]
+
+
+@pytest.mark.asyncio
+async def test_no_lock():
+    pool = await create_actor_pool("127.0.0.1", n_process=1)
+    addr = pool.external_address
+
+    # DummyActor calls ``test_method`` of WorkActor in ``do`` method,
+    # while WorkerActor depends on DummyActor ref to complete ``test_method``.
+    # If there is no ``no_lock`` decorator, dead lock happens.
+    class DummyActor(Actor):
+        def __init__(self):
+            super().__init__()
+            self.arg_list = []
+            self.kwarg_list = []
+
+        @classmethod
+        def default_uid(cls):
+            return "DummyActor"
+
+        @extensible
+        @no_lock
+        def method(self, *args, **kwargs):
+            self.arg_list.append(tuple(a * 2 for a in args))
+            self.kwarg_list.append({k: v * 2 for k, v in kwargs.items()})
+            return len(self.kwarg_list)
+
+        @method.batch
+        @no_lock
+        def b_method(self, args_list, kwargs_list):
+            self.arg_list.extend([tuple(a * 2 + 1 for a in args) for args in args_list])
+            self.kwarg_list.extend(
+                [{k: v * 2 + 1 for k, v in kwargs.items()} for kwargs in kwargs_list]
+            )
+            return [len(self.kwarg_list)] * len(args_list)
+
+        async def do(self):
+            ref = await actor_ref(address=self.address, uid=WorkerActor.default_uid())
+            await ref.test_method()
+
+        async def do_batch(self):
+            ref = await actor_ref(address=self.address, uid=WorkerActor.default_uid())
+            await ref.test_method_batch()
+
+    class WorkerActor(Actor):
+        def __init__(self):
+            super().__init__()
+
+        @classmethod
+        def default_uid(cls):
+            return "WorkerActor"
+
+        async def __post_create__(self):
+            self._dummy_ref = await actor_ref(
+                address=self.address, uid=DummyActor.default_uid()
+            )
+
+        async def test_method(self):
+            assert await self._dummy_ref.method(1, kwarg=2) == 1
+            assert getattr(DummyActor.method, NO_LOCK_ATTRIBUTE_HINT, False) is True
+
+        async def test_method_batch(self):
+            assert await self._dummy_ref.method.batch(
+                self._dummy_ref.method.delay(1, kwarg=2),
+                self._dummy_ref.method.delay(3, kwarg=4),
+            ) == [3, 3]
+
+    ref = await create_actor(DummyActor, address=addr, uid=DummyActor.default_uid())
+    await create_actor(WorkerActor, address=addr, uid=WorkerActor.default_uid())
+    await ref.do()
+    await ref.do_batch()
