7b814d81de377b95356278ac47e039cfff43dbd1
codingl2k1 <codingl2k1@outlook.com>
2023-09-12 12:19:37 +0800

    Skip coverage

--- python/xoscar/serialization/pyfury.py
+++ python/xoscar/serialization/pyfury.py
@@ -17,7 +17,7 @@ def register_classes(*args):
 def get_fury():
     if os.environ.get("USE_FURY") in ("1", "true", "True"):
         instance = getattr(_fury, "instance", None)
-        if instance is _fury_not_installed:
+        if instance is _fury_not_installed:  # pragma: no cover
             return None
         if instance is not None:
             return instance
@@ -28,7 +28,7 @@ def get_fury():
                 _fury.instance = instance = pyfury.Fury(
                     language=pyfury.Language.PYTHON, require_class_registration=False
                 )
-                for c in _register_class_list:
+                for c in _register_class_list:  # pragma: no cover
                     instance.register_class(c)
                 print("pyfury is enabled.")
             except ImportError:
