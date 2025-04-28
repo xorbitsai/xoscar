31160695927ff071ae1de76ed35d55c3a05ae849
codingl2k1 <codingl2k1@outlook.com>
2025-04-28 21:57:49 +0200

    Add debug code

--- python/xoscar/backends/indigen/tests/test_indigen_actor_context.py
+++ python/xoscar/backends/indigen/tests/test_indigen_actor_context.py
@@ -250,11 +250,21 @@ async def actor_pool(request):
         else:
             set_debug_options(None)
 
-        await pool.start()
-        try:
-            yield pool
-        finally:
-            await pool.stop()
+        import sys
+
+        if sys.version_info >= (3, 11):
+            from asyncio import timeout
+        else:
+            from async_timeout import timeout
+
+        async with timeout(180):
+            await pool.start()
+            try:
+                yield pool
+            except Exception as e:
+                logger.exception("Pool context error: %s", e)
+            finally:
+                await pool.stop()
     finally:
         set_debug_options(None)
 
